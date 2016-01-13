3daad2527b3f1cf200dadf2920c664f63cac720d
Filipe Calasans <filipe.calasans@gmail.com>
2016-01-13 01:27:21 -0200

    add main test bench

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "circular_fifo.h"
+
+#include <stdio.h>
+
+#define FIFO_SIZE   1024
+
+int main () {
+    
+    CircularFifo fifo;
+    int data[FIFO_SIZE];
+
+    initCircularFifo(&fifo, (unsigned char *)data, FIFO_SIZE);
+     
+    printf("size %d\n", (int)getSize(&fifo));
+    printf("isEmpty? %d\n", (int)isEmpty(&fifo));
+    printf("num elements %d\n", (int)getNumElements(&fifo));
+    
+    push(&fifo, 8);
+    push(&fifo, 9);
+    push(&fifo, 10);
+    push(&fifo, 11);
+    push(&fifo, 12);
+
+    printf("pop: %d\n", (int)pop(&fifo));
+    printf("pop: %d\n", (int)pop(&fifo));
+    printf("pop: %d\n", (int)pop(&fifo));
+    printf("pop: %d\n", (int)pop(&fifo));
+   
+    printf("size %d\n", (int)getSize(&fifo));
+    printf("isEmpty? %d\n", (int)isEmpty(&fifo));
+    printf("num elements %d\n", (int)getNumElements(&fifo));
+    
+    push(&fifo, 13);
+    printf("pop: %d\n", (int)pop(&fifo));
+    push(&fifo, 14);
+    printf("pop: %d\n", (int)pop(&fifo));
+    push(&fifo, 15);
+    printf("pop: %d\n", (int)pop(&fifo));
+    printf("pop: %d\n", (int)pop(&fifo));
+    
+    printf("size %d\n", (int)getSize(&fifo));
+    printf("isEmpty? %d\n", (int)isEmpty(&fifo));
+    printf("num elements %d\n", (int)getNumElements(&fifo));
+    
+    return 0;
+}
